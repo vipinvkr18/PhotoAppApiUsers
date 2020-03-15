67b2521e2b4ed81b533b401b91c1d34b002de8d9
vipinkr <vipinvkr5@gmail.com>
2020-03-15 21:08:23 +0530

    Add Spring Security Configuration and encrypt password

--- src/main/java/com/example/demo/service/UsersServiceImpl.java
+++ src/main/java/com/example/demo/service/UsersServiceImpl.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.demo.data.UserEntity;
 import com.example.demo.data.UsersRepository;
@@ -14,18 +15,20 @@ import com.example.demo.shared.UserDto;
 public class UsersServiceImpl implements UsersService{
 	UsersRepository usersRepository;
 	Environment environment;
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	public UsersServiceImpl(UsersRepository usersRepository, 
-			Environment environment)
+			Environment environment,BCryptPasswordEncoder bCryptPasswordEncoder)
 	{
 		this.usersRepository = usersRepository;
 		this.environment = environment;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		// TODO Auto-generated method stub
 		userDetails.setUserId(UUID.randomUUID().toString());
-		userDetails.setEncryptedPassword("test");
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		
 		ModelMapper modelMapper = new ModelMapper(); 
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
