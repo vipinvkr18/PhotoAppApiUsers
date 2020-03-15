4c330222915e5048e90b95153f7b23ffa9a86ac7
vipinkr <vipinvkr5@gmail.com>
2020-03-15 21:13:52 +0530

    Restrict to IP address of Zuul API Gateway

--- src/main/java/com/example/demo/security/WebSecurity.java
+++ src/main/java/com/example/demo/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+	private Environment environment;
+
+	@Autowired
+	public WebSecurity(Environment environment)
+	{
+		this.environment = environment;
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
 		http.headers().frameOptions().disable();
 	}
 
