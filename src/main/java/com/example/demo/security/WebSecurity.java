20d60355050cdbbfceb2e1edc75b1eb857b209ca
vipinkr <vipinvkr5@gmail.com>
2020-03-16 18:34:30 +0530

    Create LoginRequestModel, implement AuthentificationFilter

--- src/main/java/com/example/demo/security/WebSecurity.java
+++ src/main/java/com/example/demo/security/WebSecurity.java
@@ -3,14 +3,14 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-
+	
 	private Environment environment;
 
 	@Autowired
@@ -18,11 +18,21 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	{
 		this.environment = environment;
 	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
+		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+		.and()
+		.addFilter(getAuthenticationFilter());
 		http.headers().frameOptions().disable();
+	}
+	
+	private AuthenticationFilter getAuthenticationFilter() throws Exception
+	{
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+		authenticationFilter.setAuthenticationManager(authenticationManager());
+		return authenticationFilter;
 	}
 
 }
