3695370a328c97c58f8b90939e8e67ead2b8d086
vipinkr <vipinvkr5@gmail.com>
2020-03-26 17:10:44 +0530

    Update WebSecurity.java

--- src/main/java/com/example/demo/security/WebSecurity.java
+++ src/main/java/com/example/demo/security/WebSecurity.java
@@ -30,7 +30,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+		http.authorizeRequests()
+		.antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+		.requestMatchers(EndpointRequest.to("mappings")).hasIpAddress(environment.getProperty("gateway.ip"))
 		.and()
 		.addFilter(getAuthenticationFilter());
 		http.headers().frameOptions().disable();
