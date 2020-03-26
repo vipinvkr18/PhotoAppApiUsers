9165354e84d25c38296a770273e41eb6aea48fe7
vipinkr <vipinvkr5@gmail.com>
2020-03-26 17:26:23 +0530

    Enabled Feign Client Logging

--- src/main/java/com/example/demo/PhotoAppApiUsersApplication.java
+++ src/main/java/com/example/demo/PhotoAppApiUsersApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import feign.Logger;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -27,5 +29,10 @@ public class PhotoAppApiUsersApplication {
 	public RestTemplate getRestTemplate()
 	{
 		return new RestTemplate();
+	}
+	@Bean
+	Logger.Level feignLoggerLevel()
+	{
+		return Logger.Level.FULL;
 	}
 }
